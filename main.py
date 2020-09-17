d25e87209ac66bdc9c3f219f129d8dc1bf459a2a
Marcin <marcin.majeran13@gmail.com>
2020-09-17 21:38:56 +0200

    Added app's main structure

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+from kivy.utils import get_color_from_hex
+from kivy.core.window import Window
+from kivy.lang import Builder
+
+Window.clearcolor = get_color_from_hex('#454343')
+
+
+class WelcomeWindow(Screen, FloatLayout):
+    pass
+
+
+class MainWindow(Screen, BoxLayout):
+    pass
+
+
+class ResultWindow(Screen, BoxLayout):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+builder = Builder.load_file('pizzacalc.kv')
 
 
 class PizzaCalcApp(App):
     def build(self):
-        return FloatLayout()
+        return builder
 
 
 if __name__ == '__main__':
